dc99c216153e52cc699001f34f3fe478ad362dcc
Chathula <schathula@gmail.com>
2018-02-21 14:05:51 +0530

    clean & removed unused packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,8 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var less = require('gulp-less');
-var concat = require('gulp-concat');
-var minify = require('gulp-minify-css');
-var merge = require('merge-stream')
 var concatCss = require('gulp-concat-css');
 let cleanCSS = require('gulp-clean-css');
-
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
@@ -21,18 +16,14 @@ gulp.task('sass', function () {
 
 gulp.task('bundle', function () {
   return gulp.src('css/**/*.css')
-    .pipe(concatCss("bundle.css"))
+    .pipe(concatCss("bundle.min.css"))
     .pipe(gulp.dest('build/'));
 });
 
 gulp.task('minify-css', () => {
-  return gulp.src('build/bundle.css')
+  return gulp.src('build/bundle.min.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('dist'));
 });
 
 gulp.task('build', gulp.series('sass', 'bundle', 'minify-css'));
-
-gulp.task('sass:watch', function () {
- gulp.watch('./sass/**/*.scss', ['sass']);
-});
